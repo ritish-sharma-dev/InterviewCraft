b732a0cda8f197554526d0e605a88b5dcba4c86a
Ritish Sharma <ritishsharma04022006@gmail.com>
2026-08-28 02:32:10 +0530

    feat(models): add User and Session models

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,32 +1,30 @@
-import express from "express";  
-import cors from "cors";
-import { ENV } from "./lib/env.js";
+import express from 'express';
+import cors from 'cors';
+import { ENV } from './lib/env.js';
 
-import { connectDB } from "./lib/db.js";
-
+import { connectDB } from './lib/db.js';
 
 const app = express();
-
 
 // MIDDLEWARES
 app.use(express.json());
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 
-
 // API CHECK ROUTE
-app.get("/api/check", (req, res) => {
-  res.status(200).json({ msg: "api is up and running" });
+app.get('/api/check', (req, res) => {
+    res.status(200).json({ msg: 'api is up and running' });
 });
 
-
 // SERVER START
-const startServer = async () =>{
+const startServer = async () => {
     try {
         await connectDB();
-        app.listen(ENV.PORT, () => console.log("Server is running on port: ", ENV.PORT));
+        app.listen(ENV.PORT, () =>
+            console.log('Server is running on port: ', ENV.PORT),
+        );
     } catch (error) {
-        console.log("Error starting server: ", error);
+        console.log('Error starting server: ', error);
     }
-}
+};
 
 startServer();
