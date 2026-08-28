a2d168c629a0fcf7c361013caa4fb9afdd024d38
Ritish Sharma <ritishsharma04022006@gmail.com>
2026-08-28 18:19:30 +0530

    Deployment Configurations

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import { ENV } from './lib/env.js';
 
+import path from 'path';
+
 import { serve } from 'inngest/express';
 import { inngest, functions } from './lib/inngest.js';
 
 import { connectDB } from './lib/db.js';
 
 const app = express();
+const __dirname = path.resolve();
 
 // MIDDLEWARES
 app.use(express.json());
@@ -16,14 +19,23 @@ app.use('/api/inngest', serve({ client: inngest, functions }));
 
 // API CHECK ROUTE
 app.get('/api/check', (req, res) => {
-    res.status(200).json({ msg: 'api is up and running' });
+    res.status(200).json({ msg: 'API is up and running' });
 });
+
+// MAKE APP READY FOR DEPLOYMENT
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+    });
+}
 
 // SERVER START
 const startServer = async () => {
     try {
         await connectDB();
-        app.listen(ENV.PORT, () =>
+        app.listen(ENV.PORT || 3000, () =>
             console.log('Server is running on port: ', ENV.PORT),
         );
     } catch (error) {
